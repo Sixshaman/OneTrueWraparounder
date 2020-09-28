818d6df63be24efd6eb876468f3f1876d286150c
Sixshaman <Shaman1234@gmail.com>
2020-09-28 15:48:58 +0300

    Added color picker

--- js/Wraparounder.js
+++ js/Wraparounder.js
@@ -3,16 +3,124 @@ function wholeMod(num, modulo)
     return ((num % modulo) + modulo) % modulo;
 }
 
-function Init() 
+function hexToRGBA(hexValue)
+{
+    let hexRegex    = /^#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})$/i;
+    let parseResult = hexRegex.exec(hexValue);
+
+    if(parseResult && parseResult.length === 3 + 1)
+    {
+        let r = parseInt(parseResult[1], 16);
+        let g = parseInt(parseResult[2], 16);
+        let b = parseInt(parseResult[3], 16);
+
+        return [r, g, b, 255];
+    }
+    else
+    {
+        return [0, 0, 0, 0];
+    }
+}
+
+function fill(x, y, fillColor, imageWidth, imageHeight, useWraparound, /*inout*/ imagecontents)
+{
+    let currPixel   = y * imageWidth + x;
+    let sourceColor = [imagecontents[4 * currPixel + 0], imagecontents[4 * currPixel + 1], imagecontents[4 * currPixel + 2], imagecontents[4 * currPixel + 3]];
+
+    if(fillColor[0] === sourceColor[0] && fillColor[1] === sourceColor[1] && fillColor[2] === sourceColor[2])
+    {
+        return;
+    }
+
+    let currPixelIndices = new Set([currPixel]);
+    while(currPixelIndices.size != 0)
+    {
+        let neighbourPixelIndices = new Set([]);
+        for(const currPixelIndex of currPixelIndices)
+        {
+            let currX = Math.floor(currPixelIndex % imageHeight);
+            let currY = Math.floor(currPixelIndex / imageHeight);
+
+            let leftX   = currX - 1;
+            let rightX  = currX + 1;
+            let topY    = currY - 1;
+            let bottomY = currY + 1;
+
+            if(useWraparound)
+            {
+                leftX   = wholeMod(leftX,   imageWidth);
+                rightX  = wholeMod(rightX,  imageWidth);
+                topY    = wholeMod(topY,    imageHeight);
+                bottomY = wholeMod(bottomY, imageHeight);
+            }
+
+            let leftElementIndex   = currY   * imageWidth + leftX;
+            let rightElementIndex  = currY   * imageWidth + rightX;
+            let topElementIndex    = topY    * imageWidth + currX;
+            let bottomElementIndex = bottomY * imageWidth + currX;
+
+            if(leftX >= 0)
+            {
+                if(imagecontents[4 * leftElementIndex + 0] === sourceColor[0] && imagecontents[4 * leftElementIndex + 1] === sourceColor[1] && imagecontents[4 * leftElementIndex + 2] === sourceColor[2])
+                {
+                    neighbourPixelIndices.add(leftElementIndex);
+                }
+            }
+
+            if(rightX < imageWidth)
+            {
+                if(imagecontents[4 * rightElementIndex + 0] === sourceColor[0] && imagecontents[4 * rightElementIndex + 1] === sourceColor[1] && imagecontents[4 * rightElementIndex + 2] === sourceColor[2])
+                {
+                    neighbourPixelIndices.add(rightElementIndex);
+                }
+            }
+
+            if(topY >= 0)
+            {
+                if(imagecontents[4 * topElementIndex + 0] === sourceColor[0] && imagecontents[4 * topElementIndex + 1] === sourceColor[1] && imagecontents[4 * topElementIndex + 2] === sourceColor[2])
+                {
+                    neighbourPixelIndices.add(topElementIndex);
+                }
+            }
+
+            if(bottomY < imageHeight)
+            {
+                if(imagecontents[4 * bottomElementIndex + 0] === sourceColor[0] && imagecontents[4 * bottomElementIndex + 1] === sourceColor[1] && imagecontents[4 * bottomElementIndex + 2] === sourceColor[2])
+                {
+                    neighbourPixelIndices.add(bottomElementIndex);
+                }
+            }
+        }
+
+        for(const currPixelIndex of currPixelIndices)
+        {
+            imagecontents[4 * currPixelIndex + 0] = fillColor[0];
+            imagecontents[4 * currPixelIndex + 1] = fillColor[1];
+            imagecontents[4 * currPixelIndex + 2] = fillColor[2];
+            imagecontents[4 * currPixelIndex + 3] = fillColor[3];
+        }
+
+        currPixelIndices = neighbourPixelIndices;
+    }
+}
+
+function main() 
 {
     let canvas        = document.getElementById("CanvasWraparounder");
     let canvasContext = canvas.getContext("2d");
+
+    let imageUpload = document.getElementById("UploadImage");
+
+    let buttonSave = document.getElementById("ButtonSave");
+
+    let colorPicker = document.getElementById("ColorSelect");
 
     let imageWidth  = 0;
     let imageHeight = 0;
 
     let useWraparound = true;
 
+    //Temporary workaround, will be fixed
     canvas.width  = window.innerWidth;
     canvas.height = window.innerHeight;
     canvas.hidden = true; 
@@ -24,90 +132,12 @@ function Init()
 
         let contents = canvasContext.getImageData(0, 0, imageWidth, imageHeight);
 
-        let fillColor   = [255, 0, 0, 255]; 
-        let sourceColor = canvasContext.getImageData(x, y, 1, 1).data;
-        
-        if(fillColor[0] === sourceColor[0] && fillColor[1] === sourceColor[1] && fillColor[2] === sourceColor[2])
-        {
-            canvasContext.putImageData(contents, 0, 0);
-            return;
-        }
-
-        let currPixelIndices = new Set([y * imageWidth + x]);
-        while(currPixelIndices.size != 0)
-        {
-            let neighbourPixelIndices = new Set([]);
-            for(const currPixelIndex of currPixelIndices)
-            {
-                let currX = Math.floor(currPixelIndex % imageHeight);
-                let currY = Math.floor(currPixelIndex / imageHeight);
-
-                let leftX   = currX - 1;
-                let rightX  = currX + 1;
-                let topY    = currY - 1;
-                let bottomY = currY + 1;
-
-                if(useWraparound)
-                {
-                    leftX   = wholeMod(leftX,   imageWidth);
-                    rightX  = wholeMod(rightX,  imageWidth);
-                    topY    = wholeMod(topY,    imageHeight);
-                    bottomY = wholeMod(bottomY, imageHeight);
-                }
-
-                let leftElementIndex   = currY   * imageWidth + leftX;
-                let rightElementIndex  = currY   * imageWidth + rightX;
-                let topElementIndex    = topY    * imageWidth + currX;
-                let bottomElementIndex = bottomY * imageWidth + currX;
-
-                if(leftX >= 0)
-                {
-                    if(contents.data[4 * leftElementIndex + 0] === sourceColor[0] && contents.data[4 * leftElementIndex + 1] === sourceColor[1] && contents.data[4 * leftElementIndex + 2] === sourceColor[2])
-                    {
-                        neighbourPixelIndices.add(leftElementIndex);
-                    }
-                }
-
-                if(rightX < imageWidth)
-                {
-                    if(contents.data[4 * rightElementIndex + 0] === sourceColor[0] && contents.data[4 * rightElementIndex + 1] === sourceColor[1] && contents.data[4 * rightElementIndex + 2] === sourceColor[2])
-                    {
-                        neighbourPixelIndices.add(rightElementIndex);
-                    }
-                }
-
-                if(topY >= 0)
-                {
-                    if(contents.data[4 * topElementIndex + 0] === sourceColor[0] && contents.data[4 * topElementIndex + 1] === sourceColor[1] && contents.data[4 * topElementIndex + 2] === sourceColor[2])
-                    {
-                        neighbourPixelIndices.add(topElementIndex);
-                    }
-                }
-
-                if(bottomY < imageHeight)
-                {
-                    if(contents.data[4 * bottomElementIndex + 0] === sourceColor[0] && contents.data[4 * bottomElementIndex + 1] === sourceColor[1] && contents.data[4 * bottomElementIndex + 2] === sourceColor[2])
-                    {
-                        neighbourPixelIndices.add(bottomElementIndex);
-                    }
-                }
-            }
-
-            for(const currPixelIndex of currPixelIndices)
-            {
-                contents.data[4 * currPixelIndex + 0] = fillColor[0];
-                contents.data[4 * currPixelIndex + 1] = fillColor[1];
-                contents.data[4 * currPixelIndex + 2] = fillColor[2];
-                contents.data[4 * currPixelIndex + 3] = fillColor[3];
-            }
-
-            currPixelIndices = neighbourPixelIndices;
-        }
+        let fillColor = hexToRGBA(colorPicker.value); 
+        fill(x, y, fillColor, contents.width, contents.height, useWraparound, contents.data);
 
         canvasContext.putImageData(contents, 0, 0);
     }
 
-    let imageUpload = document.getElementById("UploadImage");
     imageUpload.onchange = function()
     {
         let imageFile = imageUpload.files[0];
@@ -120,6 +150,8 @@ function Init()
             let image    = new Image();
             image.onload = function()
             {
+                buttonSave.hidden = false;
+
                 canvas.clientWidth  = image.width;
                 canvas.clientHeight = image.height;
 
@@ -134,5 +166,13 @@ function Init()
         fileReader.readAsDataURL(imageFile);
     }
     
-    
+    buttonSave.onclick = function()
+    {
+        let link      = document.createElement("a");
+        link.href     = canvas.toDataURL("image/png");
+        link.download = "Image.png";
+
+        link.click();
+        link.remove();
+    }
 }
